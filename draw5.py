6c0f79c009fec052c8962caea4b6290d09c874a7
Erik Lunna <eslunna@gmail.com>
2016-02-20 11:55:58 -0600

    Renamed gameround to rnd.

--- draw5.py
+++ draw5.py
@@ -8,7 +8,7 @@ import card
 import os
 import game
 import blinds
-import gameround
+import rnd
 
 
 def is_integer(num):
@@ -181,7 +181,7 @@ def auto_discard(hand):
 class Draw5Game(game.Game):
     def play(self):
         """ Defines the structure of a hand played in the game."""
-        newround = gameround.Round(self)
+        newround = rnd.Round(self)
         newround.cheat_check()
 
         # todo: Postblinds
