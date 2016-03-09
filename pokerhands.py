22dea07797cf6d118ae3af43e8962a56089c2870
Erik Lunna <eslunna@gmail.com>
2016-03-08 22:05:20 -0600

    Renamed the pokerhands to shorter and more readable names. Also added high and low versions of dealt hands for testing.

--- pokerhands.py
+++ pokerhands.py
@@ -22,121 +22,126 @@ def deal_duplicates():
     return [card.Card(x[0], x[1]) for x in dupes]
 
 
-def deal_royalflush():
+def royalflush():
     rf = [('A', 's'), ('K', 's'), ('J', 's'), ('T', 's'), ('Q', 's')]
     return [card.Card(x[0], x[1]) for x in rf]
 
 
-def deal_straightflush_A():
+def straightflush_high():
     sf = [('9', 's'), ('K', 's'), ('Q', 's'), ('J', 's'), ('T', 's')]
     return [card.Card(x[0], x[1]) for x in sf]
 
 
-def deal_lowstraightflush():
+def straightflush_low():
     sf = [('2', 's'), ('A', 's'), ('3', 's'), ('4', 's'), ('5', 's')]
     return [card.Card(x[0], x[1]) for x in sf]
 
 
-def deal_4ofakind_A():
-    quads = [('A', 's'), ('A', 'd'), ('A', 'h'), ('A', 'c'), ('8', 'c')]
+def quads_high():
+    quads = [('A', 's'), ('A', 'd'), ('A', 'h'), ('A', 'c'), ('K', 'c')]
     return [card.Card(x[0], x[1]) for x in quads]
 
 
-def deal_4ofakind_B():
-    quads = [('8', 's'), ('8', 'd'), ('8', 'h'), ('8', 'c'), ('A', 'c')]
+def quads_low():
+    quads = [('2', 's'), ('2', 'd'), ('2', 'h'), ('2', 'c'), ('3', 'c')]
     return [card.Card(x[0], x[1]) for x in quads]
 
 
-def deal_fullhouse_A():
+def boat_high():
     fullhouse = [('A', 's'), ('A', 'd'), ('A', 'h'), ('K', 'd'), ('K', 'c')]
     return [card.Card(x[0], x[1]) for x in fullhouse]
 
 
-def deal_fullhouse_B():
-    fullhouse = [('K', 's'), ('K', 'd'), ('K', 'h'), ('A', 'd'), ('A', 'c')]
+def boat_low():
+    fullhouse = [('2', 's'), ('2', 'd'), ('2', 'h'), ('3', 'd'), ('3', 'c')]
     return [card.Card(x[0], x[1]) for x in fullhouse]
 
 
-def deal_flush():
-    flush = [('A', 's'), ('3', 's'), ('5', 's'), ('9', 's'), ('7', 's')]
+def flush_high():
+    flush = [('A', 's'), ('K', 's'), ('Q', 's'), ('J', 's'), ('9', 's')]
     return [card.Card(x[0], x[1]) for x in flush]
 
 
-def deal_high_straight():
+def flush_low():
+    flush = [('2', 's'), ('3', 's'), ('4', 's'), ('5', 's'), ('7', 's')]
+    return [card.Card(x[0], x[1]) for x in flush]
+
+
+def straight_high():
     straight = [('A', 's'), ('T', 'c'), ('K', 's'), ('J', 'd'), ('Q', 'h')]
     return [card.Card(x[0], x[1]) for x in straight]
 
 
-def deal_mid_straight():
+def straight_mid():
     straight = [('8', 's'), ('7', 'h'), ('9', 's'), ('T', 'd'), ('J', 'h')]
     return [card.Card(x[0], x[1]) for x in straight]
 
 
-def deal_low_straight():
+def straight_low():
     straight = [('2', 's'), ('3', 'h'), ('A', 's'), ('4', 'd'), ('5', 'c')]
     return [card.Card(x[0], x[1]) for x in straight]
 
 
-def deal_3ofakind_A():
+def set_high():
     trips = [('A', 's'), ('K', 'h'), ('A', 'h'), ('A', 'd'), ('Q', 'c')]
     return [card.Card(x[0], x[1]) for x in trips]
 
 
-def deal_3ofakind_B():
-    trips = [('K', 's'), ('Q', 'h'), ('K', 'h'), ('K', 'd'), ('A', 'c')]
+def set_low():
+    trips = [('2', 's'), ('2', 'h'), ('2', 'h'), ('3', 'd'), ('4', 'c')]
     return [card.Card(x[0], x[1]) for x in trips]
 
 
-def deal_twopair_A():
-    twopair = [('K', 's'), ('8', 'h'), ('A', 's'), ('A', 'd'), ('K', 'c')]
+def twopair_high():
+    twopair = [('A', 's'), ('A', 'h'), ('K', 's'), ('K', 'd'), ('Q', 'c')]
     return [card.Card(x[0], x[1]) for x in twopair]
 
 
-def deal_twopair_B():
-    twopair = [('K', 's'), ('A', 'h'), ('8', 's'), ('8', 'd'), ('K', 'c')]
+def twopair_low():
+    twopair = [('2', 's'), ('2', 'h'), ('3', 's'), ('3', 'd'), ('4', 'c')]
     return [card.Card(x[0], x[1]) for x in twopair]
 
 
-def deal_pair_A():
-    pair = [('2', 's'), ('3', 'h'), ('A', 's'), ('A', 'd'), ('5', 'c')]
+def pair_high():
+    pair = [('K', 's'), ('Q', 'h'), ('A', 's'), ('A', 'd'), ('J', 'c')]
     return [card.Card(x[0], x[1]) for x in pair]
 
 
-def deal_pair_B():
-    pair = [('A', 's'), ('3', 'h'), ('2', 's'), ('2', 'd'), ('5', 'c')]
+def pair_low():
+    pair = [('2', 's'), ('3', 'h'), ('2', 'c'), ('4', 'd'), ('5', 'c')]
     return [card.Card(x[0], x[1]) for x in pair]
 
 
-def deal_OESFD():
+def OESFD():
     pair = [('J', 's'), ('T', 's'), ('8', 's'), ('2', 'd'), ('9', 's')]
     return [card.Card(x[0], x[1]) for x in pair]
 
 
-def deal_GSSFD():
+def GSSFD():
     pair = [('J', 's'), ('T', 's'), ('7', 's'), ('2', 'd'), ('9', 's')]
     return [card.Card(x[0], x[1]) for x in pair]
 
 
-def deal_flushdraw_hi():
+def flushdrawA():
     pair = [('A', 's'), ('T', 's'), ('7', 's'), ('2', 'd'), ('9', 's')]
     return [card.Card(x[0], x[1]) for x in pair]
 
 
-def deal_flushdraw():
+def flushdrawB():
     pair = [('3', 's'), ('T', 's'), ('7', 's'), ('2', 'd'), ('9', 's')]
     return [card.Card(x[0], x[1]) for x in pair]
 
 
-def deal_OESD():
+def OESD():
     pair = [('J', 'h'), ('T', 's'), ('8', 'c'), ('2', 'd'), ('9', 's')]
     return [card.Card(x[0], x[1]) for x in pair]
 
 
-def deal_GSSD():
+def GSSD():
     pair = [('J', 'h'), ('T', 's'), ('A', 's'), ('2', 'd'), ('K', 'h')]
     return [card.Card(x[0], x[1]) for x in pair]
 
 
-def deal_wheeldraw():
+def wheeldraw():
     pair = [('3', 'h'), ('4', 's'), ('A', 's'), ('2', 'd'), ('K', 'h')]
     return [card.Card(x[0], x[1]) for x in pair]
--- test_pokerhands.py
+++ test_pokerhands.py
@@ -6,221 +6,221 @@ import pokerhands
 class TestPokerHands(unittest.TestCase):
     # Test Values
     def test_getvalue_royalflush_returns100000000000(self):
-        h = pokerhands.deal_royalflush()
+        h = pokerhands.royalflush()
         expected = 100000000000
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
     def test_gettype_royalflush_returnsROYALFLUSH(self):
-        h = pokerhands.deal_royalflush()
+        h = pokerhands.royalflush()
         val = evaluator.get_value(h)
         expected = 'ROYAL FLUSH'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
     def test_getvalue_straightflush_A_returns90900000000(self):
-        h = pokerhands.deal_straightflush_A()
+        h = pokerhands.straightflush_high()
         expected = 90900000000
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
     def test_gettype_straightflushA_returnsSTRAIGHTFLUSH(self):
-        h = pokerhands.deal_straightflush_A()
+        h = pokerhands.straightflush_high()
         val = evaluator.get_value(h)
         expected = 'STRAIGHT FLUSH'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
     def test_getvalue_lowstraightflush_returns90000000000(self):
-        h = pokerhands.deal_lowstraightflush()
+        h = pokerhands.straightflush_low()
         expected = 90000000000
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
     def test_gettype_lowstraightflush_returnsSTRAIGHTFLUSH(self):
-        h = pokerhands.deal_lowstraightflush()
+        h = pokerhands.straightflush_low()
         val = evaluator.get_value(h)
         expected = 'STRAIGHT FLUSH'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
-    def test_getvalue_4ofakind_A_returns81408000000(self):
-        h = pokerhands.deal_4ofakind_A()
-        expected = 81408000000
+    def test_getvalue_quadshigh_returns81413000000(self):
+        h = pokerhands.quads_high()
+        expected = 81413000000
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
-    def test_gettype_4ofakindA_returnsQUADS(self):
-        h = pokerhands.deal_4ofakind_A()
+    def test_gettype_quadshigh_returnsQUADS(self):
+        h = pokerhands.quads_high()
         val = evaluator.get_value(h)
         expected = 'QUADS'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
-    def test_getvalue_4ofakind_B_returns80814000000(self):
-        h = pokerhands.deal_4ofakind_B()
-        expected = 80814000000
+    def test_getvalue_quadslow_returns80203000000(self):
+        h = pokerhands.quads_low()
+        expected = 80203000000
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
-    def test_gettype_4ofakindB_returnsQUADS(self):
-        h = pokerhands.deal_4ofakind_B()
+    def test_gettype_quadslow_returnsQUADS(self):
+        h = pokerhands.quads_low()
         val = evaluator.get_value(h)
         expected = 'QUADS'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
-    def test_getvalue_fullhouse_A_returns71413000000(self):
-        h = pokerhands.deal_fullhouse_A()
+    def test_getvalue_boathigh_returns71413000000(self):
+        h = pokerhands.boat_high()
         expected = 71413000000
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
-    def test_gettype_fullhouseA_returnsFULLHOUSE(self):
-        h = pokerhands.deal_fullhouse_A()
+    def test_gettype_boathigh_returnsFULLHOUSE(self):
+        h = pokerhands.boat_high()
         val = evaluator.get_value(h)
         expected = 'FULL HOUSE'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
-    def test_getvalue_fullhouse_B_returns71314000000(self):
-        h = pokerhands.deal_fullhouse_B()
-        expected = 71314000000
+    def test_getvalue_boatlow_returns70203000000(self):
+        h = pokerhands.boat_low()
+        expected = 70203000000
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
-    def test_gettype_fullhouseB_returnsFULLHOUSE(self):
-        h = pokerhands.deal_fullhouse_B()
+    def test_gettype_boatlow_returnsFULLHOUSE(self):
+        h = pokerhands.boat_low()
         val = evaluator.get_value(h)
         expected = 'FULL HOUSE'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
-    def test_getvalue_flush_returns61409070503(self):
-        h = pokerhands.deal_flush()
-        expected = 61409070503
+    def test_getvalue_flushhigh_returns61413121109(self):
+        h = pokerhands.flush_high()
+        expected = 61413121109
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
-    def test_gettype_flush_returns(self):
-        h = pokerhands.deal_flush()
+    def test_gettype_flushhigh_returns(self):
+        h = pokerhands.flush_high()
         val = evaluator.get_value(h)
         expected = 'FLUSH'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
-    def test_getvalue_high_straight_returns51413121110(self):
-        h = pokerhands.deal_high_straight()
+    def test_getvalue_straighthigh_returns51413121110(self):
+        h = pokerhands.straight_high()
         expected = 51413121110
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
-    def test_gettype_high_straight_returnsSTRAIGHT(self):
-        h = pokerhands.deal_high_straight()
+    def test_gettype_straighthigh_returnsSTRAIGHT(self):
+        h = pokerhands.straight_high()
         val = evaluator.get_value(h)
         expected = 'STRAIGHT'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
-    def test_getvalue_mid_straight_returns51110090807(self):
-        h = pokerhands.deal_mid_straight()
+    def test_getvalue_straightmid_returns51110090807(self):
+        h = pokerhands.straight_mid()
         expected = 51110090807
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
-    def test_gettype_mid_straight_returnsSTRAIGHT(self):
-        h = pokerhands.deal_mid_straight()
+    def test_gettype_straightmid_returnsSTRAIGHT(self):
+        h = pokerhands.straight_mid()
         val = evaluator.get_value(h)
         expected = 'STRAIGHT'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
-    def test_getvalue_low_straight_returns50000000000(self):
-        h = pokerhands.deal_low_straight()
+    def test_getvalue_straightlow_returns50000000000(self):
+        h = pokerhands.straight_low()
         expected = 50000000000
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
-    def test_gettype_low_straight_returnsSTRAIGHT(self):
-        h = pokerhands.deal_low_straight()
+    def test_gettype_straightlow_returnsSTRAIGHT(self):
+        h = pokerhands.straight_low()
         val = evaluator.get_value(h)
         expected = 'STRAIGHT'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
-    def test_getvalue_3ofakind_A_returns41413120000(self):
-        h = pokerhands.deal_3ofakind_A()
+    def test_getvalue_sethigh_returns41413120000(self):
+        h = pokerhands.set_high()
         expected = 41413120000
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
-    def test_gettype_3ofakind_A_returnsTRIPS(self):
-        h = pokerhands.deal_3ofakind_A()
+    def test_gettype_sethigh_returnsTRIPS(self):
+        h = pokerhands.set_high()
         val = evaluator.get_value(h)
         expected = 'TRIPS'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
-    def test_getvalue_3ofakind_B_returns41314120000(self):
-        h = pokerhands.deal_3ofakind_B()
-        expected = 41314120000
+    def test_getvalue_setlow_returns40204030000(self):
+        h = pokerhands.set_low()
+        expected = 40204030000
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
-    def test_gettype_3ofakind_B_returnsTRIPS(self):
-        h = pokerhands.deal_3ofakind_B()
+    def test_gettype_setlow_returnsTRIPS(self):
+        h = pokerhands.set_low()
         val = evaluator.get_value(h)
         expected = 'TRIPS'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
-    def test_getvalue_twopairA_returns31413080000(self):
-        h = pokerhands.deal_twopair_A()
-        expected = 31413080000
+    def test_getvalue_twopairhigh_returns31413120000(self):
+        h = pokerhands.twopair_high()
+        expected = 31413120000
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
-    def test_gettype_twopairA_returnsTWOPAIR(self):
-        h = pokerhands.deal_twopair_A()
+    def test_gettype_twopairhigh_returnsTWOPAIR(self):
+        h = pokerhands.twopair_high()
         val = evaluator.get_value(h)
         expected = 'TWO PAIR'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
-    def test_getvalue_twopairB_returns31308140000(self):
-        h = pokerhands.deal_twopair_B()
-        expected = 31308140000
+    def test_getvalue_twopairlow_returns30302040000(self):
+        h = pokerhands.twopair_low()
+        expected = 30302040000
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
-    def test_gettype_twopair_B_returnsTWOPAIR(self):
-        h = pokerhands.deal_twopair_B()
+    def test_gettype_twopairlow_returnsTWOPAIR(self):
+        h = pokerhands.twopair_low()
         val = evaluator.get_value(h)
         expected = 'TWO PAIR'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
-    def test_getvalue_pairB_returns21405030200(self):
-        h = pokerhands.deal_pair_A()
-        expected = 21405030200
+    def test_getvalue_pairhigh_returns21413121100(self):
+        h = pokerhands.pair_high()
+        expected = 21413121100
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
-    def test_gettype_pair_A_returnsPAIR(self):
-        h = pokerhands.deal_pair_A()
+    def test_gettype_pairhigh_returnsPAIR(self):
+        h = pokerhands.pair_high()
         val = evaluator.get_value(h)
         expected = 'PAIR'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
-    def test_getvalue_pairA_returns20214050300(self):
-        h = pokerhands.deal_pair_B()
-        expected = 20214050300
+    def test_getvalue_pairlow_returns20205040300(self):
+        h = pokerhands.pair_low()
+        expected = 20205040300
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
-    def test_gettype_pair_B_returnsPAIR(self):
-        h = pokerhands.deal_pair_B()
+    def test_gettype_pairlow_returnsPAIR(self):
+        h = pokerhands.pair_low()
         val = evaluator.get_value(h)
         expected = 'PAIR'
         result = evaluator.get_type(val)
@@ -228,91 +228,91 @@ class TestPokerHands(unittest.TestCase):
 
     # Draws
     def test_getvalue_OESFD_returns1110090802(self):
-        h = pokerhands.deal_OESFD()
+        h = pokerhands.OESFD()
         expected = 1110090802
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
     def test_gettype__returnsHIGHCARD(self):
-        h = pokerhands.deal_OESFD()
+        h = pokerhands.OESFD()
         val = evaluator.get_value(h)
         expected = 'HIGH CARD'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
     def test_getvalue_GSSFD_returns1110090702(self):
-        h = pokerhands.deal_GSSFD()
+        h = pokerhands.GSSFD()
         expected = 1110090702
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
     def test_gettype_GSSFD_returnsHIGHCARD(self):
-        h = pokerhands.deal_GSSFD()
+        h = pokerhands.GSSFD()
         val = evaluator.get_value(h)
         expected = 'HIGH CARD'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
     def test_getvalue_flushdraw_hi_returns1410090702(self):
-        h = pokerhands.deal_flushdraw_hi()
+        h = pokerhands.flushdrawA()
         expected = 1410090702
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
     def test_gettype_flushdrawhi_returnsHIGHCARD(self):
-        h = pokerhands.deal_flushdraw_hi()
+        h = pokerhands.flushdrawA()
         val = evaluator.get_value(h)
         expected = 'HIGH CARD'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
     def test_getvalue_flushdraw_returns1009070302(self):
-        h = pokerhands.deal_flushdraw()
+        h = pokerhands.flushdrawB()
         expected = 1009070302
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
     def test_gettype_flushdraw_returnsHIGHCARD(self):
-        h = pokerhands.deal_flushdraw()
+        h = pokerhands.flushdrawB()
         val = evaluator.get_value(h)
         expected = 'HIGH CARD'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
     def test_getvalue_OESD_returns1110090802(self):
-        h = pokerhands.deal_OESD()
+        h = pokerhands.OESD()
         expected = 1110090802
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
     def test_gettype_OESD_returns(self):
-        h = pokerhands.deal_OESD()
+        h = pokerhands.OESD()
         val = evaluator.get_value(h)
         expected = 'HIGH CARD'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
     def test_getvalue_GSSD_returns1413111002(self):
-        h = pokerhands.deal_GSSD()
+        h = pokerhands.GSSD()
         expected = 1413111002
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
     def test_gettype_GSSD_returnsHIGHCARD(self):
-        h = pokerhands.deal_GSSD()
+        h = pokerhands.GSSD()
         val = evaluator.get_value(h)
         expected = 'HIGH CARD'
         result = evaluator.get_type(val)
         self.assertEqual(expected, result)
 
     def test_getvalue_wheeldraw_returns1413040302(self):
-        h = pokerhands.deal_wheeldraw()
+        h = pokerhands.wheeldraw()
         expected = 1413040302
         result = evaluator.get_value(h)
         self.assertEqual(expected, result)
 
     def test_gettype_wheeldraw_returnsHIGHCARD(self):
-        h = pokerhands.deal_wheeldraw()
+        h = pokerhands.wheeldraw()
         val = evaluator.get_value(h)
         expected = 'HIGH CARD'
         result = evaluator.get_type(val)
