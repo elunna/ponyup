f56f3fdf66512307f55185027eaa538747bd900e
Erik Lunna <eslunna@gmail.com>
2016-03-12 15:43:47 -0600

    Fixed the draw5.py module bug with the rankdict calls.

--- draw5.py
+++ draw5.py
@@ -97,7 +97,7 @@ def auto_discard(hand):
     DIS_RANKS = ['PAIR', 'TRIPS', 'QUADS']
     discard = []
 
-    h = ev.rank_dict(hand.cards)
+    h = ev.rankdict_tolist(ev.rank_dict(hand.cards))
 
     if hand.handrank in PAT_HANDS:
         pass
