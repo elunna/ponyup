d5cf07399aa0024ef4a10fb90f8b0bdf5194cf60
Erik Lunna <eslunna@gmail.com>
2016-04-17 14:08:35 -0500

    Added setUp and tests to test_gameround: do_players_have_cards, dealcards.

--- test_gameround.py
+++ test_gameround.py
@@ -1,5 +1,8 @@
 import unittest
+import blinds
+import draw5
 import gameround
+import test_table
 
 
 class TestGameRound(unittest.TestCase):
@@ -7,43 +10,94 @@ class TestGameRound(unittest.TestCase):
     Setup a table filled with 6 players for testing.
     """
     def setUp(self):
-        pass
+        STAKES = blinds.limit['50/100']
+        g = draw5.Draw5Game('FIVE CARD DRAW', STAKES, 6, 'HUMAN')
+        g._table = test_table.make_table(6)
+        g._table.randomize_button()
+        self.r = gameround.Round(g)
 
     """
     Tests for __init__ and table construction
     """
-    # initialized with invalid seat count(less than 2)
-    def test_raiseException(self):
-        pass
-        #  self.assertRaises(ValueError, table.Table, '1')
-
-    # a Test
-    def test_(self):
-        pass
 
     """
     Tests for __str__()
     """
+    # New round - pot displayed as $0
+    def test_str_newround_returns0(self):
+        expected = 'Pot: $0'
+        result = str(self.r)
+        self.assertEqual(expected, result)
 
     """
-    Tests for cheat_check()
+    Tests for do_players_have_cards():
     """
+    # Players have no cards, returns False
+    def test_doplayershavecards_nocards_returnsFalse(self):
+        expected = False
+        result = self.r.do_players_have_cards()
+        self.assertEqual(expected, result)
+
+    # 1 player has cards, returns True
+    def test_doplayershavecards_allhavecards_returnsTrue(self):
+        self.r.deal_cards(1)
+        expected = True
+        result = self.r.do_players_have_cards()
+        self.assertEqual(expected, result)
 
     """
     Tests for deal_cards(qty, faceup=False)
     """
+    # 6 players, deal 1 - should be 6 cardholders
+    def test_dealcards_deal1to6players_6cardholders(self):
+        self.r.deal_cards(1)
+        expected = 6
+        result = len(self.r.tbl.get_cardholders())
+        self.assertEqual(expected, result)
+
+    # 6 players, deal 1 - decksize == 48
+    def test_dealcards_deal1to6players_decksize48(self):
+        self.r.deal_cards(1)
+        expected = 46
+        result = len(self.r.d)
+        self.assertEqual(expected, result)
+
+    # 6 players, deal 1 (no keyword arg) - cards are hidden
+    def test_dealcards_deal1to6players_cardsarehidden(self):
+        self.r.deal_cards(1)
+        for s in self.r.tbl:
+            expected = 0
+            result = len(s.get_upcards())
+            self.assertEqual(expected, result)
+
+    # 6 players, deal 1 (faceup=True) - cards are faceup
+    def test_dealcards_deal1to6players_faceup_cardsarenothidden(self):
+        self.r.deal_cards(1, faceup=True)
+        for s in self.r.tbl:
+            self.assertFalse(s._hand.cards[0].hidden)
 
     """
     Tests for muck_all_cards()
     """
+    # 6 players, deal 1 - no cardholders after running
+    # 6 players, deal 1 - decksize == 0 after running
+    # 6 players, deal 1 - muck size == 52 after running
+    # 6 players, deal 1 - do_players_have_cards == False after running
 
     """
     Tests for verify_muck()
     """
+    # All cards mucked, returns True
+    # All cards mucked, but 1 card in deck, returns False
+    # All cards mucked, but 1 player w cards, returns False
+    # All cards mucked, but card in muck deleted, returns False
 
     """
     Tests for remove_broke_players()
     """
+    # 6 players, all with chips, returns empty list
+    # 1 broke player, returns the player in a list
+    # 6 players, all broke, table is empty.
 
     """
     Tests for get_valueplayer_list()
