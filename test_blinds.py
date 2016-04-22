43e3fd7ff43111f5da58eb94c5db0788146567eb
Erik Lunna <eslunna@gmail.com>
2016-04-22 18:37:57 -0500

    Changed the tests to only test the limit side and removed the specific blind structure passing when creating the Blinds obj.

--- test_blinds.py
+++ test_blinds.py
@@ -7,36 +7,57 @@ class TestTable(unittest.TestCase):
     Setup a table filled with 6 players for testing.
     """
     def setUp(self):
-        self.b = blinds.Blinds(blinds.limit)
+        self.b = blinds.Blinds()
 
     """
     Tests for __init__
     """
 
-    # trying to use a limit outside out the bounds of the blind_dictionary, raise exception
-    def test_init_level0_raiseException(self):
-        self.assertRaises(ValueError, blinds.Blinds, blinds.limit, 0)
-
-    def test_init_level100_raiseException(self):
-        self.assertRaises(ValueError, blinds.Blinds, blinds.limit, 1000)
-
+    """
+    Tests for set_level(level)
+    """
     def test_setlevel_limit1_hascorrectblinds(self):
         self.assertEqual(self.b.SB, 1)
         self.assertEqual(self.b.BB, 2)
         self.assertEqual(self.b.ANTE, 0)
 
-    def test_sbtoanteratio_sb150_ante25_returns6(self):
-        self.b = blinds.Blinds(blinds.WSOP, 4)
-        expected = 6
+    # trying to use a limit outside out the bounds of the blind_dictionary, raise exception
+    def test_setlevel_level0_raiseException(self):
+        self.assertRaises(ValueError, self.b.set_level, 0)
+
+    def test_setlevel_level100_raiseException(self):
+        self.assertRaises(ValueError, self.b.set_level, 1000)
+
+    """
+    Tests for sb_to_ante_ratio()
+    """
+    def test_sbtoanteratio_sb1_ante0_returns0(self):
+        self.b = blinds.Blinds(1)
+        expected = 0
         result = self.b.sb_to_ante_ratio()
         self.assertEqual(expected, result)
 
-    def test_sbtoanteratio_sb1000_ante300_returns3_3(self):
-        self.b = blinds.Blinds(blinds.WSOP, 12)
-        expected = 3.3
+    def test_sbtoanteratio_sb1_ante1_returns1(self):
+        self.b = blinds.Blinds(2)
+        expected = 1
         result = self.b.sb_to_ante_ratio()
         self.assertEqual(expected, result)
 
+    def test_sbtoanteratio_sb2_ante1_returns2(self):
+        self.b = blinds.Blinds(3)
+        expected = 2
+        result = self.b.sb_to_ante_ratio()
+        self.assertEqual(expected, result)
+
+    def test_sbtoanteratio_sb3_ante2_returns1pt5(self):
+        self.b = blinds.Blinds(6)
+        expected = 1.7
+        result = self.b.sb_to_ante_ratio()
+        self.assertEqual(expected, result)
+
+    """
+    Tests for stakes()
+    """
     def test_stakes_noAnte_returnsString(self):
         expected = '$2/$4'
         result = self.b.stakes()
