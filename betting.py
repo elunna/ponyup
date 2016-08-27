97d37cf375ea11cdcd47dc0446e5298ba08b0b85
Erik Lunna <eslunna@gmail.com>
2016-08-27 18:00:39 -0400

    Added tests and error checking for betting.calc_odds

--- betting.py
+++ betting.py
@@ -1,9 +1,6 @@
 def calc_odds(bet, pot):
     """ Calculate the odds offered to a player given a bet amount and a pot amount."""
-    print('first draft')
-    print('Bet = {}, pot = {}'.format(bet, pot))
-    print('bet is {}% of the pot'.format(bet/pot * 100))
-
+    if bet < 0 or pot < 0:
+        raise ValueError('bet or pot must be positive!')
     odds = pot / bet
-    print('The odds are {}-to-1'.format(odds))
     return odds
---
+++ test_betting.py
@@ -0,0 +1,22 @@
+import unittest
+import betting
+
+
+class TestBetting(unittest.TestCase):
+    """
+    Tests for calc_odds(bet, pot):
+    """
+
+    # bet cannot be negative
+    def test_calcodds_negbet_raiseEx(self):
+        self.assertRaises(ValueError, betting.calc_odds, -10, 10)
+
+    # pot cannot be negative
+    def test_calcodds_negpot_raiseEx(self):
+        self.assertRaises(ValueError, betting.calc_odds, 10, -10)
+
+    # bet = 5, pot = 10, we are getting 2-to-1 odds.
+    def test_init_0cardspassed_length0(self):
+        expected = 2.0
+        result = betting.calc_odds(5, 10)
+        self.assertEqual(expected, result)
