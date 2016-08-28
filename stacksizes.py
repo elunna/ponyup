819a2102c245c994f96db8e33aeb0fe103ec3943
Erik Lunna <eslunna@gmail.com>
2016-08-28 12:44:24 -0400

    Added the outline of a stacksizes module with stubs for functions and tests.

---
+++ stacksizes.py
@@ -0,0 +1,21 @@
+# Stack sizes module
+
+
+def largest(table):
+    """ Return the largest stack size at the table."""
+    pass
+
+
+def smallest(table):
+    """ Return the smallest stack size at the table."""
+    pass
+
+
+def average(table):
+    """ Return the average stack size at the table."""
+    pass
+
+
+def effective(table):
+    """ Return the effetive stack size at the table. (Note: It's also the smallest."""
+    pass
---
+++ test_stacksizes.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class TestStacksizes(unittest.TestCase):
+    """
+    Tests for def test_largest(table):
+    """
+
+    """
+    Tests for def test_smallest(table):
+    """
+
+    """
+    Tests for def test_average(table):
+    """
+
+    """
+    Tests for def test_effective(table):
+    """
