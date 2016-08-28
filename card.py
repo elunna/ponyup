c2ca22bf8f9a32f630c77007cb6c0d57da63e28d
Erik Lunna <eslunna@gmail.com>
2016-08-27 20:55:07 -0400

    Added colored suits to the card module.

--- card.py
+++ card.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+import colors
 
 # Use tuples instead of lists
 SUITS = ('c', 'd', 'h', 's')
@@ -6,6 +6,8 @@ SUITS = ('c', 'd', 'h', 's')
 # Fancy way of creating the card/value dictionary.
 FACECARDS = {'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14, 'Z': 15}
 RANKS = dict({str(x): x for x in range(2, 10)}, **FACECARDS)
+
+COLORS = {'c': 'green', 'd': 'blue', 'h': 'red', 's': 'white'}
 
 
 class Card:
@@ -24,12 +26,12 @@ class Card:
 
     def __str__(self):
         if self.hidden:
-            return 'Xx'
-        else:
-            return self.rank + self.suit
+            return colors.color('Xx', 'gray')
+
+        return colors.color(self.rank + self.suit, COLORS[self.suit])
 
     def __repr__(self):
-        return str(self)
+        return self.rank + self.suit
 
     def __eq__(self, other):
         return self.rank == other.rank and self.suit == other.suit
