f0d1565ad1bb71446c1d9300133c784cd05299f5
Erik Lunna <eslunna@gmail.com>
2016-08-30 01:33:17 -0400

    Filled in stacks.py functions and tests.

--- stacks.py
+++ stacks.py
@@ -3,22 +3,23 @@
 
 def largest(table):
     """ Return the largest stack size at the table."""
-    pass
+    return max(stacklist(table))
 
 
 def smallest(table):
     """ Return the smallest stack size at the table."""
-    pass
+    return min(stacklist(table))
 
 
 def average(table):
     """ Return the average stack size at the table."""
-    pass
+    _stacks = stacklist(table)
+    return sum(_stacks) / len(_stacks)
 
 
 def effective(table):
     """ Return the effetive stack size at the table. (Note: It's also the smallest."""
-    pass
+    return smallest(table)
 
 
 def stacklist(table):
--- test_stacks.py
+++ test_stacks.py
@@ -1,19 +1,51 @@
 import unittest
+import setup_table
 import stacks
+
 
 class TestStacks(unittest.TestCase):
     """
     Tests for def test_largest(table):
     """
+    # For 4 players, 100, 200, 300, 400.
+    def test_largest_4players_returns400(self):
+        seats = 4
+        t = setup_table.allin_table(seats)
+        expected = 400
+        result = stacks.largest(t)
+        self.assertEqual(expected, result)
 
     """
     Tests for def test_smallest(table):
     """
+    # For 4 players, 100, 200, 300, 400.
+    def test_smallest_4players_returns400(self):
+        seats = 4
+        t = setup_table.allin_table(seats)
+        expected = 100
+        result = stacks.smallest(t)
+        self.assertEqual(expected, result)
 
     """
     Tests for def test_average(table):
     """
+    def test_average_4players_returns400(self):
+        seats = 4
+        t = setup_table.allin_table(seats)
+        expected = 250
+        result = stacks.average(t)
+        self.assertEqual(expected, result)
 
     """
     Tests for def test_effective(table):
     """
+
+    """
+    Tests for stacklist(table)
+    """
+    def test_average_4players_returns4stacks(self):
+        seats = 4
+        t = setup_table.allin_table(seats)
+        expected = [100, 200, 300, 400]
+        result = stacks.stacklist(t)
+        self.assertEqual(expected, result)
