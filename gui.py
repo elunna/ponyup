e1bec5e0b397c44d819c5f97d71dba5458bee8bd
Erik Lunna <eslunna@gmail.com>
2016-09-03 15:42:50 -0400

    Added a working quit and poker msg button to the GUI. Uses standard tkinter dialogs.

--- gui.py
+++ gui.py
@@ -2,7 +2,10 @@
 
 #  from tkinter import *
 import tkinter as tk
+import tkinter.messagebox as mb
 import sys
+
+errmsg = 'POKER HAS NOT YET BEEN IMPLEMENTED!\nBILL GATES PWNS U!!!!!!'
 
 
 def quit():
@@ -10,8 +13,12 @@ def quit():
     sys.exit()
 
 
-def greeting():
-    print('Hello stdout world!...')
+def callback():
+    if mb.askyesno('verify', 'do you really want to quit?'):
+        quit()
+        #  mb.showwarning('Yes', 'Quit not yet implemented')
+    else:
+        mb.showinfo('No', 'Quit has been cancelled')
 
 root = tk.Tk()
 labelfont = ('times', 20, 'bold')
@@ -21,5 +28,10 @@ title.config(bg='black', fg='yellow')
 title.config(font=labelfont)
 title.config(height=3, width=30)
 title.pack(side=tk.TOP)
+
+tk.Button(text='Quit', command=callback).pack(fill=tk.X)
+tk.Button(text='Poker', command=(lambda: mb.showerror('Pokerz', errmsg))).pack(fill=tk.X)
+
+buttons = tk.Frame()
 
 root.mainloop()
