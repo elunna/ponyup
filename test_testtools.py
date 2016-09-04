dfe49058a0d1f73088eb87385d53d5be70c277b5
Erik Lunna <eslunna@gmail.com>
2016-09-03 23:08:47 -0400

    Moved the table factorys to the testtools module.

--- test_testtools.py
+++ test_testtools.py
@@ -1,26 +1,26 @@
 import unittest
-import table_factory
+import testtools
 
 
-class TestTableFactory(unittest.TestCase):
+class TestTestTools(unittest.TestCase):
 
     """
     Tests BobTable
     """
     def test_bobtable_2seats_len2(self):
-        t = table_factory.BobTable(2)
+        t = testtools.BobTable(2)
         expected = 2
         result = len(t)
         self.assertEqual(expected, result)
 
     def test_bobtable_2seats_1000chips(self):
-        t = table_factory.BobTable(2)
+        t = testtools.BobTable(2)
         expected = 1000
         result = t.seats[0].chips
         self.assertEqual(expected, result)
 
     def test_bobtable_2seats_bob0_in_seat0(self):
-        t = table_factory.BobTable(2)
+        t = testtools.BobTable(2)
         expected = 'bob0'
         result = str(t.seats[0])
         self.assertEqual(expected, result)
@@ -29,13 +29,13 @@ class TestTableFactory(unittest.TestCase):
     Tests SteppedStackTable
     """
     def test_steppedstacktable_seat0_100chips(self):
-        t = table_factory.SteppedStackTable(2)
+        t = testtools.SteppedStackTable(2)
         expected = 100
         result = t.seats[0].chips
         self.assertEqual(expected, result)
 
     def test_steppedstacktable_seat1_200chips(self):
-        t = table_factory.SteppedStackTable(2)
+        t = testtools.SteppedStackTable(2)
         expected = 200
         result = t.seats[1].chips
         self.assertEqual(expected, result)
--- testtools.py
+++ testtools.py
@@ -164,3 +164,35 @@ def get_dealt_table(hands):
     for k, v in hands.items():
         t.seats[k]._hand.cards = pokerhands.convert_to_cards(v)
     return t
+
+"""
+Table factory.
+"""
+STARTINGCHIPS = 1000
+STEP = 100
+
+
+class BobTable(table.Table):
+    """
+    Creates a table of bobs with the default chip stack.
+    """
+    def __init__(self, seats):
+        super().__init__(seats)
+
+        for i, s in enumerate(self.seats):
+            self.add_player(i, player.Player('bob{}'.format(i), 'CPU'))
+            self.seats[i].chips = STARTINGCHIPS
+            #  s.chips = STARTINGCHIPS
+
+
+class SteppedStackTable(table.Table):
+    """
+    Creates a table of bobs with stack sizes starting from 100 and increasing in 100's for
+    each seat.
+    """
+    def __init__(self, seats):
+        super().__init__(seats)
+
+        for i, s in enumerate(self.seats):
+            self.add_player(i, player.Player('bob{}'.format(i), 'CPU'))
+            self.seats[i].chips = STEP * (i + 1)
