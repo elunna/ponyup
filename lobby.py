9c681909aa8e7647c8e0e598d399ea49f882e51d
Erik Lunna <eslunna@gmail.com>
2016-09-05 17:56:14 -0400

    Added a draft version of a lobby with fun pony names.

---
+++ lobby.py
@@ -0,0 +1,95 @@
+"""
+This lobby listing is a list of all the available cash tables a pony can play
+at.
+Each has:
+    * A table name
+    * Table size
+    * Stakes
+    * Game type
+"""
+
+Draw games:
+
+Golden Oak Library
+Twilight's Lab
+Schoolhouse
+Train Station
+Ponyville Tower
+Fluttershy's cottage
+Ponyville Day Spa
+Rainbow Dash's House
+Sugarcube Corner
+Town Hall
+Quills and Sofas
+Hay's Hardware
+Mr. Breezy's Fan shop
+The Hay Burger
+Wonderbolt Academy
+CMC Clubhouse
+Rainbow Factory
+Apple Acres
+White Tail Woods
+Froggy Bottom Bogg
+Ghastly Gorge
+Trixie's Wagon
+Zecora's Hut
+Cutie Map
+Twilight's Balloon
+The Pie Farm
+Carousel Boutique
+Bookshop
+Bowling Alley
+Joke Shop
+Cafe Hay
+
+Crystal Palace
+Discord's Dimension
+Griffonstone
+Yakyakistan
+
+Dragon Mountain
+Mirror Pool
+Castle of the Two Sisters
+Castle of Friendship
+
+Cloudsdale
+Our Tow
+
+
+
+
+Stud games:
+Crystal Empire
+Everfree Forest
+Canterlot
+Appleloosa
+Fillydelphia
+Foaledo
+Hoofington
+Manechester
+Las Pegasus
+Baltimare
+Tartarus
+Saddle Arabia
+Mustangia
+Manehattan
+Ponyville
+San Franciscolt
+Seaddle
+Trittingham
+Winneapolis
+Vanhoover
+Mount Everhoof
+Raket Range
+Crystal Mountains
+Neighagra Falls
+Friffish Islaes
+Unicorn Range
+Saddle Lake
+Horseshoe Bay
+San Palomino Desert
+
+Dragon Lands
+Winsome Falls
+Tenochtitlan Basin
+Dodge Junction
