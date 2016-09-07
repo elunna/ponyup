ab588359313429875d4387d51686baffdcd278a8
Erik Lunna <eslunna@gmail.com>
2016-09-06 22:36:23 -0400

    Organized the lobby into 5draw and 5stud tables, added a bunch of other ideas to a brainstorm file and created a few lookup functions for the lobby list.

--- lobby.py
+++ lobby.py
@@ -1,95 +1,80 @@
+from collections import namedtuple
+
+Game = namedtuple('Game', ['name', 'seats', 'level', 'game'])
+
 """
 This lobby listing is a list of all the available cash tables a pony can play
-at.
-Each has:
-    * A table name
-    * Table size
-    * Stakes
-    * Game type
+at. Each has: Table name, seats, stakes level, game type.
 """
 
-Draw games:
 
-Golden Oak Library
-Twilight's Lab
-Schoolhouse
-Train Station
-Ponyville Tower
-Fluttershy's cottage
-Ponyville Day Spa
-Rainbow Dash's House
-Sugarcube Corner
-Town Hall
-Quills and Sofas
-Hay's Hardware
-Mr. Breezy's Fan shop
-The Hay Burger
-Wonderbolt Academy
-CMC Clubhouse
-Rainbow Factory
-Apple Acres
-White Tail Woods
-Froggy Bottom Bogg
-Ghastly Gorge
-Trixie's Wagon
-Zecora's Hut
-Cutie Map
-Twilight's Balloon
-The Pie Farm
-Carousel Boutique
-Bookshop
-Bowling Alley
-Joke Shop
-Cafe Hay
-
-Crystal Palace
-Discord's Dimension
-Griffonstone
-Yakyakistan
-
-Dragon Mountain
-Mirror Pool
-Castle of the Two Sisters
-Castle of Friendship
-
-Cloudsdale
-Our Tow
+def sort_by_name(L):
+    return sorted(L, key=lambda x: x.name)
 
 
+def sort_by_level(L):
+    return sorted(L, key=lambda x: x.level)
 
 
-Stud games:
-Crystal Empire
-Everfree Forest
-Canterlot
-Appleloosa
-Fillydelphia
-Foaledo
-Hoofington
-Manechester
-Las Pegasus
-Baltimare
-Tartarus
-Saddle Arabia
-Mustangia
-Manehattan
-Ponyville
-San Franciscolt
-Seaddle
-Trittingham
-Winneapolis
-Vanhoover
-Mount Everhoof
-Raket Range
-Crystal Mountains
-Neighagra Falls
-Friffish Islaes
-Unicorn Range
-Saddle Lake
-Horseshoe Bay
-San Palomino Desert
+def get_games(L, game):
+    return [x for x in L if x.game == game]
 
-Dragon Lands
-Winsome Falls
-Tenochtitlan Basin
-Dodge Junction
+# TABLENAME | TABLE SIZE | STAKES | GAME
+lobby = (
+    # Headsup
+    Game('Twilight\'s Balloon', seats=2, level=1, game="FIVE CARD DRAW"),
+    Game('Rainbow Dash\'s House', seats=2, level=2, game="FIVE CARD DRAW"),
+    Game('Ponyville Tower', seats=2, level=3, game="FIVE CARD DRAW"),
+    Game('Zecora\'s Hut', seats=2, level=4, game="FIVE CARD DRAW"),
+    Game('Trixie\'s Wagon', seats=2, level=5, game="FIVE CARD DRAW"),
+
+    # 3max
+    Game('Twilight\'s Lab', seats=3, level=1, game="FIVE CARD DRAW"),
+    Game('CMC Clubhouse', seats=3, level=2, game="FIVE CARD DRAW"),
+    Game('Mr. Breezy\'s Fan shop', seats=3, level=3, game="FIVE CARD DRAW"),
+    Game('Fluttershy\'s cottage', seats=3, level=4, game="FIVE CARD DRAW"),
+    Game('Quills and Sofas', seats=3, level=5, game="FIVE CARD DRAW"),
+
+    # 6max
+    Game('Golden Oak Library', seats=6, level=1, game="FIVE CARD DRAW"),
+    Game('Carousel Boutique', seats=6, level=2, game="FIVE CARD DRAW"),
+    Game('Sugarcube Corner', seats=6, level=3, game="FIVE CARD DRAW"),
+    Game('Pinkie\'s Party Cavet', seats=6, level=4, game="FIVE CARD DRAW"),
+    Game('Cutie Map', seats=6, level=5, game="FIVE CARD DRAW"),
+
+    # 8max
+    Game('Apple Acres', seats=8, level=1, game="FIVE CARD DRAW"),
+    Game('Ponyville Schoolhouse', seats=8, level=2, game="FIVE CARD DRAW"),
+    Game('Wonderbolt Academy', seats=8, level=3, game="FIVE CARD DRAW"),
+    Game('Rainbow Factory', seats=8, level=4, game="FIVE CARD DRAW"),
+    Game('Mirror Pool', seats=8, level=5, game="FIVE CARD DRAW"),
+
+    # ### 5-card stud
+    # HU
+    Game('Everfree Forest', seats=2, level=1, game="FIVE CARD STUD"),
+    Game('White Tail Woods', seats=2, level=2, game="FIVE CARD STUD"),
+    Game('Galloping Gorge', seats=2, level=3, game="FIVE CARD STUD"),
+    Game('Froggy Bottom Bogg', seats=2, level=4, game="FIVE CARD STUD"),
+    Game('Dragon Lands', seats=2, level=5, game="FIVE CARD STUD"),
+
+    # 3max
+    Game('Raket Range', seats=3, level=1, game="FIVE CARD STUD"),
+    Game('Crystal Mountains', seats=3, level=2, game="FIVE CARD STUD"),
+    Game('Friffish Islaes', seats=3, level=3, game="FIVE CARD STUD"),
+    Game('Unicorn Range', seats=3, level=4, game="FIVE CARD STUD"),
+    Game('Saddle Lake', seats=3, level=5, game="FIVE CARD STUD"),
+
+    # 6max
+    Game('Horseshoe Bay', seats=6, level=1, game="FIVE CARD STUD"),
+    Game('San Palomino Desert', seats=6, level=2, game="FIVE CARD STUD"),
+    Game('Dragon Lands', seats=6, level=3, game="FIVE CARD STUD"),
+    Game('Winsome Falls', seats=6, level=4, game="FIVE CARD STUD"),
+    Game('Tenochtitlan Basin', seats=6, level=4, game="FIVE CARD STUD"),
+
+    # 8max
+    Game('Mount Everhoof', seats=8, level=1, game="FIVE CARD STUD"),
+    Game('Ghastly Gorge', seats=8, level=2, game="FIVE CARD STUD"),
+    Game('Hollow Shades', seats=8, level=3, game="FIVE CARD STUD"),
+    Game('Dodge Junction', seats=8, level=4, game="FIVE CARD STUD"),
+    Game('Tartarus', seats=8, level=5, game="FIVE CARD STUD"),
+)
