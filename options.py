c571835bf2c8e05bcb86567bfe2b4d87a9dce671
Erik Lunna <eslunna@gmail.com>
2016-09-07 00:00:21 -0400

    Added a simple options.py file for setting game options.

---
+++ options.py
@@ -0,0 +1,22 @@
+"""
+Game options:
+todo: Speed
+todo: Rabbit cam
+todo: Debug mode
+todo: Enable odds calculator
+todo: Toggle hand strength display
+
+
+Debug mode: Shows betting activity.
+God-mode: Shows all cards,
+"""
+
+
+OPTIONS = {
+    'rabbit': False,
+    'debug': False,
+    'odds': True,
+    'rank': True,
+    'godmode': False,
+    'speed': 1,
+}
