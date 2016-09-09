17b2b38566a8524215030d85743ed9aa5db9617a
Erik Lunna <eslunna@gmail.com>
2016-09-09 19:52:51 -0400

    Added the Pinochle deck and test.

---
+++ pinochle_deck.py
@@ -0,0 +1,13 @@
+import deck
+import card
+
+
+class PinochleDeck(deck.Deck):
+    """
+    Creates a 48 card Pinochle deck with 2 of each rank: 9, T, J, Q, K, A.
+    """
+    def __init__(self):
+        pinochle_cards = ['9', 'T', 'J', 'Q', 'K', 'A']
+        c = [card.Card(r, s[0]) for s in card.SUITS for r in card.RANKS if r in pinochle_cards]
+
+        self.cards = c + c
---
+++ test_pinochledeck.py
@@ -0,0 +1,30 @@
+import unittest
+import pinochle_deck
+import card
+
+
+class TestPinochleDeck(unittest.TestCase):
+    """
+    Tests for __init__()
+    """
+    def test_init_PinochleDeck_size48(self):
+        d = pinochle_deck.PinochleDeck()
+        expected = 48
+        result = len(d)
+        self.assertEqual(expected, result)
+
+    def test_init_PinochleDeck_8Aces(self):
+        d = pinochle_deck.PinochleDeck()
+        expected = 8
+        result = 0
+        for c in d.cards:
+            if c.rank == 'A':
+                result += 1
+        self.assertEqual(expected, result)
+
+    def test_init_PinochleDeck_2AceSpades(self):
+        c = card.Card('A', 's')
+        d = pinochle_deck.PinochleDeck()
+        expected = 2
+        result = d.cards.count(c)
+        self.assertEqual(expected, result)
