26a9c3933b6e11e572f502f50cc5b82bc08c95c0
Erik Lunna <eslunna@gmail.com>
2016-09-10 10:26:19 -0400

    Made a joker module and drafted a pick_joker function that picks the best card from a deck to be the joker and get the highest hand.

---
+++ joker.py
@@ -0,0 +1,32 @@
+import card
+import deck
+import evaluator
+
+
+def pick_joker(cards):
+    """
+    Picks the best card for the joker to represent for a group of cards.
+    """
+    # If the Joker is in the pile of cards, remove it for analysis.
+    if card.JOKER1 in cards:
+        cards.remove(card.JOKER1)
+    if card.JOKER2 in cards:
+        cards.remove(card.JOKER2)
+
+    if len(cards) != 4:
+        raise Exception('Cards should be a group of 4 cards!')
+
+    d = deck.Deck()
+    d.unhide()
+    bestvalue = 0
+    bestcard = None
+
+    for c in d.cards:
+        testhand = cards[:]
+        testhand.append(c)
+        test_val = evaluator.get_value(testhand)
+
+        if test_val > bestvalue:
+            bestvalue = test_val
+            bestcard = c
+    return bestcard
