e0c7d5edcbc17e15d8cc6a69db00e17060147adc
Erik Lunna <eslunna@gmail.com>
2016-09-10 23:49:16 -0400

    Started an outline of a handhistory module. Imported an example hand history from full tilt.

---
+++ handhistory.py
@@ -0,0 +1,13 @@
+"""
+Hand history logger. Logs the actions that take place during a round of poker.
+
+For a normal hand history file, we will start with a header:
+        * PonyupPoker Poker Game
+        * Game ID#
+        * Table name,
+        * stakes,
+        * Game type
+        * time,
+        * date
+        * Seats, names, stack sizes
+"""
