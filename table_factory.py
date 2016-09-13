f15f861e726644129c314be347097fbbbc183bb2
Erik Lunna <eslunna@gmail.com>
2016-09-12 23:52:05 -0400

    Removed Table subclasses from table_factory and started a new factory method.

--- table_factory.py
+++ table_factory.py
@@ -3,102 +3,55 @@ import table
 import names
 
 DEPOSIT = 10000
-STARTINGCHIPS = 1000
+DEF_STACK = 1000
 STEP = 100
 
 
-class BobTable(table.Table):
-    """
-    Creates a table of generic Player objects named bob0, bob1, bob2, etc... with the default
-    chip stack.
-    """
-    def __init__(self, seats):
-        super().__init__(seats)
+def factory(**new_config):
+    config = {
+        'seats': 6,
+        'game': None,
+        'name': 'default',
+        'types': None,  # Player types
+        'names': 'bob',  # Player names, can be 'random'
+        'heroname': None,  # If there is a hero, they will be placed at the hero seat.
+        'heroseat': 0,
+        'BB': None,  # Size of the big blind.
+        'deposit': DEPOSIT,
+        'stack': DEF_STACK,
+        'stepstacks': False,
+        'stackvariation': None,
+    }
 
-        for i, s in enumerate(self.seats):
-            s.sitdown(player.Player('bob{}'.format(i), 'CPU'))
-            s.player.deposit(DEPOSIT)
-            s.buy_chips(STARTINGCHIPS)
+    config.update(new_config)
+    SEATS = config['seats']
+    t = table.Table(SEATS)
 
-
-class SteppedStackTable(table.Table):
     """
-    Creates a table of bobs with stack sizes starting from 100 and increasing in 100's for
-    each seat.
-    """
-    def __init__(self, seats):
-        super().__init__(seats)
+    # Create a list of players
+    if config['names'] == 'random':
+        # Generate random names
+        nameset = names.random_names(SEATS)
+    else:
+        nameset = [config['names'] + str(i) for i in range(SEATS)]
 
-        for i, s in enumerate(self.seats):
-            s.sitdown(player.Player('bob{}'.format(i), 'CPU'))
-            s.player.deposit(DEPOSIT)
+    # Fund and Seat the players
+    for i, s in enumerate(t):
+        p = player.Player(nameset[i], config['game'], config['types'])
+        p.deposit(config['deposit'])
+        s.sitdown(p)
+
+    # Create and place the hero player.
+    if config['heroname']:
+        hero = player.Player(config['heroname'])
+        t.seats[config['heroname']].player = hero
+
+    # Players buyin to the table.
+    if config['stepstacks']:
+        for i, s in enumerate(t):
             s.buy_chips(STEP * (i + 1))
-
-
-class Draw5Table(table.Table):
+    else:
+        for s in t:
+            s.buy_chips(DEF_STACK)
     """
-    Creates a table of bobs that are 5 Card Players, and with the default stack size.
-    """
-    def __init__(self, seats):
-        super().__init__(seats)
-
-        for i, s in enumerate(self.seats):
-            s.sitdown(player.Player5Card('bob{}'.format(i), None))
-            s.player.deposit(DEPOSIT)
-            s.buy_chips(STARTINGCHIPS)
-
-
-class Stud5Table(table.Table):
-    """
-    Creates a table of bobs that are 5 Card Players, and with the default stack size.
-    """
-    def __init__(self, seats):
-        super().__init__(seats)
-
-        for i, s in enumerate(self.seats):
-            s.sitdown(player.Player5Stud('bob{}'.format(i), None))
-            s.player.deposit(DEPOSIT)
-            s.buy_chips(STARTINGCHIPS)
-
-
-class HeroTable(table.Table):
-    """
-    Creates a table with the human hero player, and populates the table full of random named
-    players. Each player has the default starting stack size.
-    """
-    def __init__(self, seats, hero, game):
-        super().__init__(seats)
-
-        nameset = names.random_names(seats)
-        # Add the hero to seat 0
-        self.seats[0].sitdown(player.Player(hero, 'HUMAN'))
-        self.seats[0].player.deposit(DEPOSIT)
-
-        for i, s in enumerate(self.seats):
-            if s.is_empty():
-                newplayer = player_factory(game, nameset.pop())
-                s.sitdown(newplayer)
-            else:
-                nameset.pop()
-
-            s.player.deposit(DEPOSIT)
-            s.buy_chips(STARTINGCHIPS)
-
-
-def change_playertypes(table, playertype):
-    for p in table:
-        if p.playertype == "HUMAN":
-            pass
-        else:
-            p.playertype = playertype
-
-
-def player_factory(game, name, playertype=None):
-    if game == "FIVE CARD DRAW":
-        p = player.Player5Card(name, playertype)
-    elif game == "FIVE CARD STUD":
-        p = player.Player5Stud(name, playertype)
-    elif game is None:
-        p = player.Player(name, playertype="CPU")
-
-    return p
+    return t
