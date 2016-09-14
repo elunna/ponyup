88992768fd4bf10519e904dc8c921e0d178528e0
Erik Lunna <eslunna@gmail.com>
2016-09-14 01:21:36 -0400

    Moved the highhand function from stud to the table module.

--- table.py
+++ table.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import card
 import console
+import evaluator
 import random
 import seat
 
@@ -242,3 +243,34 @@ class Table():
             seats = self.get_players()
 
         return len(seats) - seats.index(seat) - 1
+
+    def highhand(self):
+        """
+        Finds which player has the highest showing hand and return their seat index.  For stud
+        games, after the first street, the high hand on board initiates the action (a tie is
+        broken by position, with the player who received cards first acting first).
+        """
+        highvalue = 0
+        seat = None
+        ties = []
+
+        for s in self.get_players(hascards=True):
+            h = s.hand.get_upcards()
+            value = evaluator.get_value(h)
+
+            if value > highvalue:
+                highvalue, seat = value, s
+                ties = [seat]  # Reset any lower ties.
+            elif value == highvalue:
+                ties.append(s)
+                if seat not in ties:
+                    ties.append(seat)
+
+        # Return the seat index of the first-to-act.
+        if len(ties) > 1:
+            # Process ties, get the player who was dealt first.
+            for s in self.get_players(hascards=True):
+                if s in ties:
+                    return s.NUM
+        else:
+            return seat.NUM
