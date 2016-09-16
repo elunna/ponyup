aae8f2a7d9f3a0eff8cc4a1ce651bc2d6dfab740
Erik Lunna <eslunna@gmail.com>
2016-09-16 13:30:30 -0400

    Added a make_playerpool function to session_factory and made tests for different pool sizes and types.

--- session_factory.py
+++ session_factory.py
@@ -1,5 +1,7 @@
 import blinds
 import draw5
+import names
+import player
 import stud
 import table_factory
 
@@ -39,3 +41,39 @@ def factory(**new_config):
     # Set tablename for the session.
     sesh.tablename = config['tablename']
     return sesh
+
+
+def make_playerpool(**new_config):
+    config = {
+        'quantity': None,
+        'game': None,
+        'types': 'random',  # Player types
+        'names': 'bob',  # Player names, can be 'random'
+        'deposit': table_factory.DEPOSIT,
+        'variance': None,   # A percentage that the deposit can randomly vary.
+    }
+    config.update(new_config)
+    qty = config['quantity']
+
+    if qty is None or qty <= 0:
+        raise ValueError('Cannot make an empty playerpool!')
+
+    # Create a list of players
+    if config['names'] == 'random':
+        # Generate random names
+        nameset = names.random_names(qty)
+    else:
+        nameset = [config['names'] + str(i) for i in range(qty)]
+
+    playerpool = []
+
+    # Create the players
+    for i, n in enumerate(nameset):
+        p = player.factory(n, config['game'], config['types'])
+        playerpool.append(p)
+
+    # Fund the players
+    for p in playerpool:
+        p.deposit(config['deposit'])
+
+    return playerpool
--- test_sessionfactory.py
+++ test_sessionfactory.py
@@ -1,5 +1,6 @@
 import unittest
 import session_factory
+import table_factory
 
 
 class TestSessionFactory(unittest.TestCase):
@@ -19,3 +20,56 @@ class TestSessionFactory(unittest.TestCase):
     """
     Tests for make_playerpool(**new_config)
     """
+    # No quantity pass
+    def test_makeplayerpool_noquantity_raisesException(self):
+        self.assertRaises(ValueError, session_factory.make_playerpool)
+
+    # 1 player
+    def test_makeplayerpool_qty1_len1(self):
+        pool = session_factory.make_playerpool(quantity=1)
+        expected = 1
+        result = len(pool)
+        self.assertEqual(expected, result)
+
+    # 1 player has the DEPOSIT amt
+    def test_makeplayerpool_qty1_hasbank(self):
+        pool = session_factory.make_playerpool(quantity=1)
+        expected = table_factory.DEPOSIT
+        result = pool[0].bank
+        self.assertEqual(expected, result)
+
+    # 1 player, is the pass playertype
+    def test_makeplayerpool_1FISH_isFISH(self):
+        ptype = 'FISH'
+        pool = session_factory.make_playerpool(quantity=1, types=ptype)
+        expected = ptype
+        result = pool[0].playertype
+        self.assertEqual(expected, result)
+
+    # 1 player, named 'bob0'
+    def test_makeplayerpool_1defaultname_isbob0(self):
+        pool = session_factory.make_playerpool(quantity=1)
+        expected = 'bob0'
+        result = pool[0].name
+        self.assertEqual(expected, result)
+
+    # 10 players
+    def test_makeplayerpool_qty10_len10(self):
+        pool = session_factory.make_playerpool(quantity=10)
+        expected = 10
+        result = len(pool)
+        self.assertEqual(expected, result)
+
+    # 10 players, bobs, test last name
+    def test_makeplayerpool_10bobs_lastisbob9(self):
+        pool = session_factory.make_playerpool(quantity=10)
+        expected = 'bob9'
+        result = pool[-1].name
+        self.assertEqual(expected, result)
+
+    # 10 players, random names, all unique
+    def test_makeplayerpool_10random_allunique(self):
+        pool = session_factory.make_playerpool(quantity=10, names='random')
+        expected = 10
+        result = len(set([p.name for p in pool]))
+        self.assertEqual(expected, result)
