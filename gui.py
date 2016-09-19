b0b0132189730f25dff47eb395f35c97fdf42f21
Erik Lunna <eslunna@gmail.com>
2016-09-18 20:57:37 -0400

    Cleaned up the gui and made the quit button into it's own class.

--- gui.py
+++ gui.py
@@ -3,35 +3,38 @@
 #  from tkinter import *
 import tkinter as tk
 import tkinter.messagebox as mb
-import sys
 
 errmsg = 'POKER HAS NOT YET BEEN IMPLEMENTED!\nBILL GATES PWNS U!!!!!!'
 
 
-def quit():
-    print("Hey I gotta get going.")
-    sys.exit()
+class QuitButton(tk.Frame):
+    def __init__(self, parent=None):
+        tk.Frame.__init__(self, parent)
+        self.pack()
+        widget = tk.Button(self, text='Quit', command=self.quit)
+        widget.pack(side=tk.LEFT, expand=tk.YES, fill=tk.BOTH)
 
+    def quit(self):
+        if mb.askyesno('verify', 'do you really want to quit?'):
+            quit()
+        else:
+            mb.showinfo('No', 'Quit has been cancelled')
 
-def callback():
-    if mb.askyesno('verify', 'do you really want to quit?'):
-        quit()
-        #  mb.showwarning('Yes', 'Quit not yet implemented')
-    else:
-        mb.showinfo('No', 'Quit has been cancelled')
+if __name__ == "__main__":
 
-root = tk.Tk()
-labelfont = ('times', 20, 'bold')
+    root = tk.Tk()
+    labelfont = ('times', 20, 'bold')
 
-title = tk.Label(root, text='Pycard$ by Erik Lunna')
-title.config(bg='black', fg='yellow')
-title.config(font=labelfont)
-title.config(height=3, width=30)
-title.pack(side=tk.TOP)
+    title = tk.Label(root, text='Pycard$ by Erik Lunna')
+    title.config(bg='black', fg='yellow')
+    title.config(font=labelfont)
+    title.config(height=3, width=30)
+    title.pack(side=tk.TOP)
 
-tk.Button(text='Quit', command=callback).pack(fill=tk.X)
-tk.Button(text='Poker', command=(lambda: mb.showerror('Pokerz', errmsg))).pack(fill=tk.X)
+    #  tk.Button(text='Quit', command=callback).pack(fill=tk.X)
+    QuitButton(root).pack()
+    tk.Button(text='Poker', command=(lambda: mb.showerror('Pokerz', errmsg))).pack(fill=tk.X)
 
-buttons = tk.Frame()
+    buttons = tk.Frame()
 
-root.mainloop()
+    root.mainloop()
