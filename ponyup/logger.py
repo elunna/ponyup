adda01e6b9a1d1ad3756380cd38f289c95c24335
Erik Lunna <eslunna@gmail.com>
2016-10-04 15:52:02 -0400

    Added logging to the Round and separated INFO logging to go to a separate info.log.

--- ponyup/logger.py
+++ ponyup/logger.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 import logging
 
-FILENAME = 'logs/poker.log'
+DEBUG_FILE = 'logs/debug.log'
+INFO_FILE = 'logs/info.log'
 
 
-def get_logger(name, filename=FILENAME):
+def get_logger(name, filename=DEBUG_FILE):
     # Logger setup
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
-    # Setup file handler
-    fh = logging.FileHandler(filename)
-    fh.setLevel(logging.DEBUG)
+    # Setup file handlers
+    debug_fh = logging.FileHandler(filename)
+    debug_fh.setLevel(logging.DEBUG)
+
+    info_fh = logging.FileHandler(INFO_FILE)
+    info_fh.setLevel(logging.INFO)
 
     # Setup console stream handler
     ch = logging.StreamHandler()
@@ -19,10 +23,12 @@ def get_logger(name, filename=FILENAME):
 
     # Setup formatting
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
+    debug_fh.setFormatter(formatter)
+    info_fh.setFormatter(formatter)
     ch.setFormatter(formatter)
 
     # Add handlers to logger
-    logger.addHandler(fh)
+    logger.addHandler(debug_fh)
+    logger.addHandler(info_fh)
     #  logger.addHandler(ch)
     return logger
--- ponyup/poker.py
+++ ponyup/poker.py
@@ -6,7 +6,10 @@ from ponyup import console
 from ponyup import deck
 from ponyup import evaluator
 from ponyup import handhistory
+from ponyup import logger
 from ponyup import pots
+
+_logger = logger.get_logger(__name__)
 
 
 class Round():
@@ -14,6 +17,8 @@ class Round():
         """
         Initialize the next round of Poker.
         """
+        _logger.debug('Initialized a new Round.')
+
         self.sesh = session
         self.gameid = random.randint(100000000, 999999999)
         self.street = 0
@@ -28,6 +33,13 @@ class Round():
         self.check_integrity_pre()
         self.hh = handhistory.HandHistory(self)
 
+        _logger.debug('Round - gameid: {}'.format(self.gameid))
+        _logger.debug('Round - street: {}'.format(self.street))
+        _logger.debug('Round - pot: {}'.format(self.pot))
+        _logger.debug('Round - muck: {}'.format(self.muck))
+        _logger.debug('Round - deck: {}'.format(self.d))
+        _logger.debug('Round - exposed: {}'.format(self.exposed))
+
     def __str__(self):
         """
         Return info about the current round.
@@ -41,6 +53,7 @@ class Round():
         try:
             return getattr(self.sesh, name)
         except AttributeError:
+            _logger.error('Error - {} attribute not in self.session'.format(name))
             raise AttributeError("Child' object has no attribute {}".format(name))
 
     def log(self, txt, echo=True, decorate=False):
@@ -51,16 +64,25 @@ class Round():
         self.hh.log(txt)
 
     def log_holecards(self):
+        _logger.debug('Logging the hero\'s hole cards.')
         self.log('Hole Cards', decorate=True, echo=False)
+        _logger.info('Hole Cards')
+
         hero = self.find_hero()
+        _logger.debug('Hero is: {}'.format(hero.player))
+
         cards = hero.hand.peek()
         self.log('{}: [{}]'.format(hero, cards.strip()), echo=False)
+        _logger.info('{}: [{}]'.format(hero, cards.strip()))
 
     def deal_cards(self, qty, faceup=False, handreq=False):
         """
         Deal the specified quantity of cards to each player. If faceup is True, the cards are
         dealt face-up, otherwise they are face-down.
         """
+        _logger.debug('Dealing cards out to table.')
+        _logger.debug('qtt={}, faceup={}, handreq={}'.format(qty, faceup, handreq))
+
         for i in range(qty):
             for s in self.table.get_players():
                 if handreq and not s.has_hand():
@@ -72,19 +94,23 @@ class Round():
 
                     if s is not self.find_hero():
                         self.log('{} was dealt [{}]'.format(s.player, c), echo=False)
+                        _logger.info('{} was dealt [{}]'.format(s.player, c))
                     self.exposed.append(c)
 
     def show_cards(self):
         """
         Unhides all player hands.
         """
+        _logger.debug('All player hands are being revealed.')
         for s in self.table.get_players(hascards=True):
+            _logger.debug('Unhiding {}\'s hand.'.format(s.player))
             s.hand.unhide()
 
     def sortcards(self):
         """
         Sort all cards in all players hands.
         """
+        _logger.debug('Sorting all player hands.')
         for s in self.table:
             s.hand.sort()
 
@@ -93,19 +119,22 @@ class Round():
         Takes the card from the seat's hand and transfers it to the muck. Returns True if the
         operation was successful, False if it didn't.
         """
+        _logger.debug('Seat {} is discarding {}.'.format(seat.NUM, c))
         self.muck.append(seat.hand.discard(c))
 
     def burn(self):
+        _logger.debug('Burning a card to the muck.')
         self.muck.append(self.d.deal())
 
     def muck_all_cards(self):
         """
         Muck all player hands, and muck the contents of the deck.
         """
-        # Clear hands
+        _logger.debug('Mucking all player hands.')
         for s in self.table:
             self.muck.extend(s.fold())
-        # Add the remainder of the deck
+
+        _logger.debug('Mucking the deck.')
         while len(self.d) > 0:
             self.muck.append(self.d.deal())
 
@@ -113,6 +142,7 @@ class Round():
         """
         All players bet the ante amount and it's added to the pot.
         """
+        _logger.debug('Making players post antes.')
         actions = ''
         for s in self.table:
             actions += '{} posts ${} ante.\n'.format(s, self.blinds.ANTE)
@@ -126,10 +156,14 @@ class Round():
         Gets the small and big blind positions from the table and makes each player bet the
         appropriate mount to the pot. Returns a string describing what the blinds posted.
         """
+        _logger.debug('Making players post blinds.')
+
         if self.table.TOKENS['D'] == -1:
+            _logger.error('Button has not been set yet!')
             raise Exception('Button has not been set yet!')
 
         if len(self.table.get_players()) < 2:
+            _logger.error('Not enough players to play!')
             raise ValueError('Not enough players to play!')
             exit()
         sb = self.table.seats[self.table.TOKENS['SB']]
@@ -150,13 +184,16 @@ class Round():
         Gets the player who must post the bringin amount, adds their bet to the pot, and
         returns a string describing what the blinds posted.
         """
+        _logger.debug('Finding the player who is the bringin.')
         table = self.table
         bi = table.TOKENS['BI']
         if bi == -1:
+            _logger.error('Bringin has not been set on the table!')
             raise Exception('Bringin has not been set on the table!')
 
         seat = table.seats[bi]
 
+        _logger.debug('Making a player post the bringin.')
         # Bet the Bringin amount and add to the pot
         self.pot += seat.bet(self.blinds.BRINGIN)
         action = ''
@@ -170,36 +207,56 @@ class Round():
         Advanced the street counter by one.
         """
         if self.street >= len(self.streets):
+            _logger.error('The last street has been reached on this game!')
             raise Exception('The last street has been reached on this game!')
         else:
+            _logger.debug('Advancing the Round to the next street.')
             self.street += 1
 
     def get_street(self):
+        _logger.debug('Returning the current Street.')
         return self.streets[self.street]
 
     def one_left(self):
         cardholders = self.table.get_players(hascards=True)
         if len(cardholders) == 1:
+            _logger.debug('There is only one seat left with cards.')
+            _logger.debug('Returning the last remaining seat')
             return cardholders.pop()
         else:
+            _logger.debug('More than one player has cards.')
             return None
 
     def betting_round(self):
         """
         Run through a round of betting. Returns a victor if it exists.
         """
+        _logger.debug('Printing console version of table.')
         print(console.display_table(self.table, self.find_hero()))
+
+        _logger.debug('Creating Betting object.')
         br = betting.BettingRound(self)
 
+        _logger.debug('Starting iteration through Betting object.')
         for p in br:
+            _logger.debug('Getting player decision.')
+
             o = br.player_decision(p)
+            _logger.debug('Player chose {}.'.format(o))
+
             br.process_option(o)
+            _logger.debug('Processed player option.')
+
             act_str = br.action_string(o)
+            _logger.debug('Created action string for player action.')
+
             space = betting.spacing(br.level())
+
             console.print_action(space, act_str)
 
             # Log every action
             self.hh.log(act_str)
+            _logger.info(act_str)
 
         console.print_pot(self.pot)
 
@@ -210,23 +267,33 @@ class Round():
         otherwise.
         """
         hands = len(self.table.get_players(hascards=True))
+        _logger.debug('There are {} seats with hands left.'.format(hands))
+
         broke = len(self.table.get_broke_players())
+        _logger.debug('There are {} broke players.'.format(broke))
+
         if hands - broke <= 1:
+            _logger.debug('The betting round is over')
             return True
         else:
+            _logger.debug('The betting round will continue.')
             return False
 
     def found_winner(self):
+        _logger.debug('Checking if a one player is left to claim the pot.')
         victor = self.one_left()
+
         if victor is None:
+            _logger.debug('No winner, proceeding to next street..')
             self.next_street()
             return False
         else:
-            # One player left, award them the pot!
+            _logger.debug('One player left.')
             oneleft = 'Only one player left!'.rjust(70)
             print(colors.color(oneleft, 'LIGHTBLUE'))
 
             awardtext = pots.award_pot(victor, self.pot.pot)
+            _logger.info(awardtext)
             self.log(awardtext)
             return True
 
@@ -236,39 +303,65 @@ class Round():
         winner the appropriate amount.
         """
         title = 'Showdown!'
+        _logger.debug(title)
         self.log(title, decorate=True, echo=False)
         console.right_align(title)
+
+        _logger.debug('Turning all player hands up.')
         self.show_cards()
 
         self.log(console.show_hands(self.table, color=False), echo=False)
         console.right_align((console.show_hands(self.table, color=True)))
 
+        _logger.debug('Calculating pots and sidepots.')
         award_txt = self.pot.allocate_money_to_winners()
+        _logger.info(award_txt)
         self.log(award_txt, echo=False)
         console.right_align(award_txt)
 
     def cleanup(self):
+        _logger.debug('Cleanup phase.')
         self.muck_all_cards()
 
         if not self.check_integrity_post():
+            _logger.error('Integrity of game could not be verified after round was complete!')
             raise Exception('Integrity of game could not be verified after round was complete!')
 
     def check_integrity_pre(self):
         """
         Verify that the game elements are set up correctly.
         """
+        _logger.debug('Checking the integrity of the game, pre-deal.')
+
         # Check that the deck is full.
         if len(self.d) != self.DECKSIZE:
+            _logger.error('Deck is corrupted, not full size.')
             return False
+
+        _logger.debug('Deck is full.')
+
         # Check that the muck is empty.
         if len(self.muck) != 0:
+            _logger.error('Muck is corrupted, has cards in it.')
             return False
+
+        _logger.debug('Muck is empty.')
+
         # Check that no players have cards.
         if len(self.table.get_players(hascards=True)) > 0:
+            _logger.error('Play is corrupted, a seat has cards pre-deal.')
             return False
+
+        _logger.debug('No players have cards.')
+
         # Check that the pot is 0.
         if self.pot != 0:
+            _logger.error('Pot is corrupted, has chips in it.')
             return False
+
+        _logger.debug('Pot is empty.')
+
+        _logger.debug('All checks pass. Pre-deal integrity intact.')
         return True
 
     def check_integrity_post(self):
@@ -276,20 +369,36 @@ class Round():
         Verify that the game elements have been cleaned up correctly and that all cards are
         accounted for.
         """
+        _logger.debug('Checking the integrity of the game, post-showdown.')
+
         # Check that all cards have been used up.
         if len(self.d) != 0:
+            _logger.error('Deck is corrupted, has cards in it.')
             return False
+
+        _logger.debug('Deck is empty.')
+
         # Check that the muck is the same size as the original starting deck.
         if len(self.muck) != self.DECKSIZE:
+            _logger.error('Muck is corrupted, doesn\'t equal starting deck size.')
             return False
+
+        _logger.debug('Muck is full.')
+
         # Check that all players have folded.
         if len(self.table.get_players(hascards=True)) > 0:
+            _logger.error('Seat is corrupted, has cards.')
             return False
+
+        _logger.debug('No players have cards.')
+
         # The sum of all sidepots should equal the potsize.
 
+        _logger.debug('All checks pass. Post-showdown integrity intact.')
         return True
 
     def exposed_cards(self):
+        _logger.debug('Finding new exposed cards to add to the Rounds list.')
         exposed = []
         for s in self.table:
             exposed.extend(s.hand.get_upcards())
@@ -301,8 +410,8 @@ class Round():
         games, after the first street, the high hand on board initiates the action (a tie is
         broken by position, with the player who received cards first acting first).
         """
-        highvalue = 0
-        seat = None
+        _logger.debug('Determining which player has the highest exposed hand.')
+        seat, highvalue = None, 0
         ties = []
 
         for s in self.table.get_players(hascards=True):
@@ -312,16 +421,24 @@ class Round():
             if value > highvalue:
                 highvalue, seat = value, s
                 ties = [seat]  # Reset any lower ties.
+                _logger.debug('Current high hand value: {}.'.format(highvalue))
+                _logger.debug('Seat with current highest hand: {}.'.format(seat.NUM))
             elif value == highvalue:
+                _logger.debug('Seat {} tied for current high value.'.format(s.NUM))
                 ties.append(s)
+
+                # Is this needed?
                 if seat not in ties:
                     ties.append(seat)
 
         # Return the seat index of the first-to-act.
         if len(ties) > 1:
+            _logger.debug('There was a tie for high hand, breaking tie with seat position')
             # Process ties, get the player who was dealt first.
             for s in self.table.get_players(hascards=True):
                 if s in ties:
+                    _logger.debug('Seat {} was dealt first, returning its index.'.format(s.NUM))
                     return s.NUM
         else:
+            _logger.debug('Seat {} has high hand, returning its index.'.format(s.NUM))
             return seat.NUM
