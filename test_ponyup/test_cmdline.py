e907851d3d56fccf5ef2516fe8fce497a43d9392
Erik Lunna <eslunna@gmail.com>
2016-10-05 22:44:20 -0400

    Renamed test_console to test_cmdline.

--- test_ponyup/test_cmdline.py
+++ test_ponyup/test_cmdline.py
@@ -1,9 +1,8 @@
 import unittest
-from ponyup import card
-from ponyup import console
+from ponyup import cmdline
 
 
-class TestConsole(unittest.TestCase):
+class TestCmdline(unittest.TestCase):
 
     """
     Tests for is_integer(num)
@@ -11,29 +10,29 @@ class TestConsole(unittest.TestCase):
     # Pass an integer 10. Returns True.
     def test_isinteger_10_returnsTrue(self):
         expected = True
-        result = console.is_integer(10)
+        result = cmdline.is_integer(10)
         self.assertEqual(expected, result)
 
     # Pass an integer 10. Returns True.
     def test_isinteger_str10_returnsTrue(self):
         expected = True
-        result = console.is_integer('10')
+        result = cmdline.is_integer('10')
         self.assertEqual(expected, result)
 
     # Pass a string. Returns False.
     def test_isinteger_string_returnsFalse(self):
         expected = False
-        result = console.is_integer('string')
+        result = cmdline.is_integer('string')
         self.assertEqual(expected, result)
 
     # Pass a float 10.5. Returns False.
     def test_isinteger_float_returnsFalse(self):
         expected = False
-        result = console.is_integer(10.5)
+        result = cmdline.is_integer(10.5)
         self.assertEqual(expected, result)
 
     # Pass an integer 10. Returns True.
     def test_isinteger_str10pt3_returnsTrue(self):
         expected = False
-        result = console.is_integer('10.3')
+        result = cmdline.is_integer('10.3')
         self.assertEqual(expected, result)
