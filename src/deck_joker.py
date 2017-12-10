f31c299292496881ad7022f8afd85647cd2ecd11
Erik Lunna <eslunna@gmail.com>
2017-12-10 13:00:38 -0600

    Moved the joker deck construction to it's own function.

--- src/deck_joker.py
+++ src/deck_joker.py
@@ -2,11 +2,14 @@ from . import deck
 from . import playingcard
 
 
+def mk_joker_deck(jokers):
+    cards = playingcard.std_deck()
+    for _ in range(jokers):
+        cards.append(playingcard.Joker())
+    return cards
+
+
 class DeckJoker(deck.Deck):
     """ Creates a deck with one Joker. """
-    def __init__(self, jokers=1):
-        # super(deck.Deck, self).__init__()
+    def __init__(self, jokers):
         deck.Deck.__init__(self)
-
-        for _ in range(jokers):
-            self.cards.append(playingcard.Joker())
--- tests/test_deck_joker.py
+++ tests/test_deck_joker.py
@@ -4,30 +4,35 @@
 from ..src import deck_joker as dj
 from ..src import playingcard as pc
 
+# Test deck constructor
 
-def test_1Joker_containsJoker():
-    d = dj.DeckJoker(jokers=1)
+
+def test_mkjokerdeck_1Joker_containsJoker():
+    d = dj.mk_joker_deck(jokers=1)
     joker1 = pc.Joker()
-    assert joker1 in d.cards
+    assert joker1 in d
 
 
-def test_1Joker_size53():
-    d = dj.DeckJoker(jokers=1)
+def test_mkjokerdeck_1Joker_size53():
+    d = dj.mk_joker_deck(jokers=1)
     assert len(d) == 53
 
 
-def test_2Joker_size54():
-    d = dj.DeckJoker(jokers=2)
+def test_mkjokerdeck_2Joker_size54():
+    d = dj.mk_joker_deck(jokers=2)
     assert len(d) == 54
 
 
-def test_1Joker_contains1Joker():
-    d = dj.DeckJoker(jokers=1)
-    count = sum(1 for c in d.cards if isinstance(c, pc.Joker))
+def test_mkjokerdeck_1Joker_contains1Joker():
+    d = dj.mk_joker_deck(jokers=1)
+    count = sum(1 for c in d if isinstance(c, pc.Joker))
     assert count == 1
 
 
-def test_2Joker_contains2Jokers():
-    d = dj.DeckJoker(jokers=2)
-    count = sum(1 for c in d.cards if isinstance(c, pc.Joker))
+def test_mkjokerdeck_2Joker_contains2Jokers():
+    d = dj.mk_joker_deck(jokers=2)
+    count = sum(1 for c in d if isinstance(c, pc.Joker))
     assert count == 2
+
+
+# Test Joker class
