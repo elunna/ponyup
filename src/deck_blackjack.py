35a21cfcfff903909a5eda37de4de773f5c3bd75
Erik Lunna <eslunna@gmail.com>
2017-12-10 12:55:09 -0600

    Moved the blackjack deck creation to a separate tested function.

--- src/deck_blackjack.py
+++ src/deck_blackjack.py
@@ -1,15 +1,17 @@
 from . import deck
+from . import playingcard as pc
+
+
+def mk_blackjack_deck(shoes):
+    if shoes < 1:
+        raise ValueError('BlackjackDeck must be passed a value of 1 or more for shoes!')
+    return pc.std_deck() * shoes
 
 
 class BlackjackDeck(deck.Deck):
     """ Creates a blackjack deck with the specified number of 'shoes' included.
         4 shoes is the most common size for a Las Vegas blackjack deck.
     """
-    def __init__(self, shoes=4):
-        if shoes < 1:
-            raise ValueError('BlackjackDeck must be passed a value of 1 or more for shoes!')
-
+    def __init__(self, shoes):
         super(BlackjackDeck, self).__init__()
-        cardset = self.cards[:]
-        for _ in range(shoes - 1):
-            self.cards.extend(cardset)
+        self.cards = mk_blackjack_deck(shoes)
--- tests/test_deck_blackjack.py
+++ tests/test_deck_blackjack.py
@@ -6,27 +6,27 @@ from ..src import deck_blackjack as bj
 from ..src import playingcard as pc
 
 
-def test_init_0shoes_raiseException():
+def test_mkblackjackdeck_raiseException():
     with pytest.raises(ValueError):
-        bj.BlackjackDeck(0)
+        bj.mk_blackjack_deck(0)
 
 
-def test_init_negshoes_raiseException():
+def test_mkblackjackdeck_negshoes_raiseException():
     with pytest.raises(ValueError):
-        bj.BlackjackDeck(-1)
+        bj.mk_blackjack_deck(-1)
+
+
+def test_init_2shoes_104cards():
+    d = bj.mk_blackjack_deck(2)
+    assert len(d) == 104
 
 
 def test_init_4shoes_208cards():
-    d = bj.BlackjackDeck(4)
+    d = bj.mk_blackjack_deck(4)
     assert len(d) == 208
 
 
 def test_init_4shoes_4AceSpaces():
-    d = bj.BlackjackDeck(4)
+    d = bj.mk_blackjack_deck(4)
     c = pc.PlayingCard('A', 's')
-    assert d.cards.count(c) == 4
-
-
-def test_init_6shoes_312cards():
-    d = bj.BlackjackDeck(6)
-    assert len(d) == 312
+    assert d.count(c) == 4
