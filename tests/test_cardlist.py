7ae1bb1b274f7352d10540cf8c2c70f6e9395765
Erik Lunna <eslunna@gmail.com>
2017-12-10 09:57:41 -0600

    Added fixture to test_cardlist for regular cardlist.

--- tests/test_cardlist.py
+++ tests/test_cardlist.py
@@ -1,78 +1,84 @@
+import pytest
 from ..src import cardlist
 from ..src import deck
 from ..src import joker
-from ..src import playingcard as pc
-from ..src import tools
+from ..src import card
 
 
-def test_len_newcardlist_size0():
-    cl = cardlist.CardList()
-    assert len(cl) == 0
+@pytest.fixture
+def full_deck():
+    d = deck.std_deck()
+    return cardlist.CardList(d)
 
 
-def test_len_2cards_len2():
-    cards = tools.convert_to_cards(['As', 'Ks'])
-    cl = cardlist.CardList(cards)
-    assert len(cl) == 2
+@pytest.fixture
+def test_cl():
+    return cardlist.CardList()
 
 
-def test_len_fulldeck_len52():
-    cl = cardlist.CardList(deck.std_deck())
-    assert len(cl) == 52
+def test_len_newcardlist_size0(test_cl):
+    assert len(test_cl) == 0
+
+
+def test_len_1card_len1():
+    c = card.Card('As')
+    cl = cardlist.CardList([c])
+    assert len(cl) == 1
+
+
+def test_len_fulldeck_len52(full_deck):
+    assert len(full_deck) == 52
 
 
 def test_str_1card():
-    c = pc.PlayingCard('A', 's')
+    c = card.Card('As')
     cl = cardlist.CardList([c])
     cl.toggle_hidden(False)
     assert str(cl) == 'As'
 
 
 def test_contains_AceSpadesinStandardDeck_returnsTrue():
-    c = pc.PlayingCard('A', 's')
+    c = card.Card('As')
     cl = cardlist.CardList([c])
     assert c in cl.cards
 
 
-def test_contains_JokerinStandardDeck_returnsFalse():
-    cl = cardlist.CardList()
+def test_contains_JokerinStandardDeck_returnsFalse(test_cl):
     c = joker.Joker()
-    assert c not in cl.cards
+    assert c not in test_cl.cards
 
 
 def test_isempty_hasacard_returnFalse():
-    c = pc.PlayingCard('A', 's')
+    c = card.Card('As')
     cl = cardlist.CardList([c])
     assert cl.is_empty() is False
 
 
-def test_isempty_emptydeck_returnTrue():
-    cl = cardlist.CardList()
-    assert cl.is_empty()
+def test_isempty_emptydeck_returnTrue(test_cl):
+    assert test_cl.is_empty()
 
 
 def test_remove_existingcard_size0():
-    c = pc.PlayingCard('A', 's')
+    c = card.Card('As')
     cl = cardlist.CardList([c])
     cl.remove(c)
     assert len(cl) == 0
 
 
-def test_remove_cardnotindeck_returnsNone():
-    cl = cardlist.CardList()
-    c = pc.PlayingCard('Z', 's')
-    assert cl.remove(c) == None
+def test_remove_cardnotindeck_returnsNone(test_cl):
+    c = card.Card('Zs')
+    assert test_cl.remove(c) == None
 
 
 def test_togglehidden_1card_hidden():
-    c = pc.PlayingCard('A', 's')
+    c = card.Card('As')
     cl = cardlist.CardList([c])
     cl.toggle_hidden(False)
     assert cl.cards[0].hidden is False
 
 
 def test_togglehidden_1card_nothidden():
-    c = pc.PlayingCard('A', 's')
+    c = card.Card('As')
     cl = cardlist.CardList([c])
     cl.toggle_hidden(True)
     assert cl.cards[0].hidden
